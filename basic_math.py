3f535e7070618a45a10ed12c8904e1ae318b4881
younginPark <youngin.park9@gmai.com>
2021-01-24 14:05:50 +0900

    week1 HW Code Review

---
+++ basic_math.py
@@ -0,0 +1,108 @@
+# -*- coding: utf8 -*-
+#######################
+# Basic Math          #
+#######################
+
+"""
+여기서 간단한 수학을 하는 프로그램을 만들것입니다. 
+"""
+
+
+def get_greatest(number_list):
+    """
+    주어진 리스트에서 가장 큰 숫자를 반환함
+
+        Parameters:
+            number_list (list): integer로 값으로만 구성된 list
+            ex - [10, 33, 22, 99, 33]
+
+        Returns:
+            greatest_number (int): parameter number_list 중 가장 큰 값
+
+        Examples:
+            >>> number_list = [39, 54, 32, 11, 99]
+            >>> import basic_math as bm
+            >>> bm.get_greatest(number_list)
+            99
+    """
+    greatest_number = 0
+    for i in range(len(number_list)):
+        if greatest_number < number_list[i]:
+            greatest_number = number_list[i]
+    return greatest_number
+
+
+def get_smallest(number_list):
+    """
+    주어진 리스트에서 제일 작은 숫자를 반환함
+
+        Parameters:
+            number_list (list): integer로 값으로만 구성된 list
+            ex - [10, 33, 22, 99, 33]
+
+        Returns:
+            smallest_number (int): parameter number_list 중 가장 작은 값
+
+        Examples:
+            >>> number_list = [39, 54, 32, 11, 99]
+            >>> import basic_math as bm
+            >>> bm.get_smallest(number_list)
+            11
+    """
+    smallest_number = number_list[0]
+    for i in range(len(number_list)):
+        if smallest_number > number_list[i]:
+            smallest_number = number_list[i]
+    return smallest_number
+
+
+def get_mean(number_list):
+    """
+    주어진 리스트 숫자들의 평균을 구함.
+
+        Parameters:
+            number_list (list): integer로 값으로만 구성된 list
+            ex - [10, 33, 22, 99, 33]
+
+        Returns:
+            mean (int): parameter number_list 숫자들의 평균
+
+        Examples:
+            >>> number_list = [39, 54, 32, 11, 99]
+            >>> import basic_math as bm
+            >>> bm.get_mean(number_list)
+            47
+    """
+    mean = 0
+    for i in range(len(number_list)):
+        mean += number_list[i]
+    mean /= len(number_list)
+    return mean
+
+
+def get_median(number_list):
+    """
+    주어진 리스트 숫자들의 중간값을 구함.
+
+        Parameters:
+            number_list (list): integer로 값으로만 구성된 list
+            ex - [10, 33, 22, 99, 33]
+
+        Returns:
+            median (int): parameter number_list 숫자들의 중간값
+
+        Examples:
+            >>> number_list = [39, 54, 32, 11, 99]
+            >>> import basic_math as bm
+            >>> bm.get_median(number_list)
+            39
+            >>> number_list2 = [39, 54, 32, 11, 99, 5]
+            >>> bm.get_median(number_list2)
+            35.5
+    """
+    number_list.sort()
+    if len(number_list) % 2 == 0:
+        median = (number_list[len(number_list)//2-1]+number_list[len(number_list)//2]) / 2
+    else:
+        median = number_list[len(number_list)//2]
+    return median
